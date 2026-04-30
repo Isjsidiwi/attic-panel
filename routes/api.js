88f5b4362d82d2f73452d2e5a1e20116440453d5
Faris <119116968+Isjsidiwi@users.noreply.github.com>
2026-04-30 23:29:41 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -2,20 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/game/MLBB', async (req, res) => {
-  const userKey = (req.body.user_key || '').trim();
-
-  res.json({
-    "success": true,
-    "session_token": "eaf8a86b70cc9b566a3a424a323d452b80ea02b77228a2c04e45d87c78455a2a",
-    "name_key": userKey,
-    "expiry_date": "2026-12-31 23:59:59",
-    "remaining_devices": "99/100",
-    "vip": "NO",
-    "file_url": null,
-    "version": "1.0",
-    "announcement": "NEW UPDATE SOON ?",
-    "seller": "lord"
-  });
+  res.json({ "success": true, "seller": "lord", "version": "1.0" });
 });
 
 module.exports = router;
